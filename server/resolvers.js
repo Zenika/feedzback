e0dfb1077d811b6be056b0d52872eba749bf9934
bnyat22 <bnyat.azizsharif@zenika.com>
2022-04-22 17:02:04 +0200

    clean code with eslint and prettier

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,13 +3,12 @@ const getEmail = require('./getEmail');
 
 
 const resolvers = {
-    Query: {
-        allMessage:  () =>  getEmail(),
-        getResult: ()=> global.result
-           
-    },
-    Mutation: {
-        createMessage: async(_,payload)=> await sendEmail(payload)
-    }
-}
+  Query: {
+    allMessage: () => getEmail(),
+    getResult: ()=> global.result,
+  },
+  Mutation: {
+    createMessage: async (_, payload)=> await sendEmail(payload),
+  },
+};
 module.exports = resolvers;
--- server/sendEmail.js
+++ server/sendEmail.js
@@ -1,58 +1,55 @@
-const sgMail = require('@sendgrid/mail')
+const sgMail = require('@sendgrid/mail');
 const {Datastore} = require('@google-cloud/datastore');
-const { getNameFromEmail } = require('./model/getNameFromEmail');
+const {getNameFromEmail} = require('./model/getNameFromEmail');
 require('dotenv').config();
 
-const datastore  = new Datastore({
-  projectId: 'feedzback-343709'
+const datastore= new Datastore({
+  projectId: 'feedzback-343709',
 });
-const insertValue = value => {
+const insertValue = (value) => {
   datastore.save({
     key: datastore.key('visit'),
-    data:value});
-    
+    data: value,
+  });
+};
 
-}
-module.exports = async ({ email, message }) => {
+module.exports = async ({email, message}) => {
+  const recever = getNameFromEmail(message.email).split(' ');
+  const envi = process.env.NODE_ENV || 'development';
+  const pointsPositifs = message.pointsPositifs.replace(/\n/g, '<br>');
+  const axesAmeliorations = message.axesAmeliorations.replace(/\n/g, '<br>');
+  const commentaire = message.commentaire.replace(/\n/g, '<br>');
 
- let recever = getNameFromEmail(message.email).split(" ");
- const envi = process.env.NODE_ENV || 'development';
- 
- let pointsPositifs = message.pointsPositifs.replace(/\n/g,"<br>");
- let axesAmeliorations = message.axesAmeliorations.replace(/\n/g,"<br>");
- let commentaire = message.commentaire.replace(/\n/g,"<br>");
- console.log(pointsPositifs);
- /*
- if(envi=='development'){
-   console.log("les données envoyées " + email + " model " )
-  return "le feedback a été envoyé(une reponse automatique en mode de developement)";
- }
-*/
-  const msg = {
-      to: email,
-      from:"feedzback@zenika.com",
-      template_id:'d-0015050451894264ba6885324349ab71',
-      dynamic_template_data:{
-        name:recever[0],
-        senderName:message.nom,
-        pointsPositifs:pointsPositifs,
-        axesAmeliorations:axesAmeliorations,
-        commentaire:commentaire
-      }
+  if (envi=='development') {
+    return 'le feedback a été envoyé(une reponse automatique en mode '+
+      'de developement)';
   }
+
+  const msg = {
+    to: email,
+    from: 'feedzback@zenika.com',
+    template_id: 'd-0015050451894264ba6885324349ab71',
+    dynamic_template_data: {
+      name: recever[0],
+      senderName: message.nom,
+      pointsPositifs: pointsPositifs,
+      axesAmeliorations: axesAmeliorations,
+      commentaire: commentaire,
+    },
+  };
 
   try {
-    
- const res = await sgMail.send(msg).then(()=>{
-   return "le feedback a été envoyé!";
- }).catch(()=>{
-  return "Le feedback n'est pas envoyé, vérifier les données s'il vous plaît";
- });
-       insertValue(msg);
-       return res;
-      
-  } catch(error){
+    const res = await sgMail.send(msg).then(()=>{
+      return 'le feedback a été envoyé!';
+    }).catch(()=>{
+      // eslint-disable-next-line quotes
+      return "Le feedback n'est pas envoyé, vérifier les données"+
+      // eslint-disable-next-line quotes
+      + "s'il vous plaît";
+    });
+    insertValue(msg);
+    return res;
+  } catch (error) {
     return error;
   }
-
-}
+};
