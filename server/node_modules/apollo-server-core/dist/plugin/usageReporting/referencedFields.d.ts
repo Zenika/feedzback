54758f7e4379424182112bd644e815941c5bc53b
bnyat22 <bnyat.azizsharif@zenika.com>
2022-05-03 17:08:25 +0200

    remove gitignore files

--- server/node_modules/apollo-server-core/dist/plugin/usageReporting/referencedFields.d.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentNode, GraphQLSchema } from 'graphql';
-import { ReferencedFieldsForType } from 'apollo-reporting-protobuf';
-export declare type ReferencedFieldsByType = Record<string, ReferencedFieldsForType>;
-export declare function calculateReferencedFieldsByType({ document, schema, resolvedOperationName, }: {
-    document: DocumentNode;
-    resolvedOperationName: string | null;
-    schema: GraphQLSchema;
-}): ReferencedFieldsByType;
-//# sourceMappingURL=referencedFields.d.ts.map
