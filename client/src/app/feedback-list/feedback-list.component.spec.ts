4a8cbdb4255186953bfcbf17fad947530caed4ee
VincentLandais <100678450+VincentLandais@users.noreply.github.com>
2022-05-30 15:37:34 +0200

    Update client/src/app/feedback-list/feedback-list.component.spec.ts
    Co-authored-by: Patrice De Saint Steban <patrice@desaintsteban.fr>

--- client/src/app/feedback-list/feedback-list.component.spec.ts
+++ client/src/app/feedback-list/feedback-list.component.spec.ts
@@ -27,7 +27,7 @@ describe('FeedbackListComponent', () => {
   });
 
   it('should display "From:" if type is "Received"', () => {
-    component.feedbacks = [new Feedback("John", "john@gmail.com", "Steve", "steve@gmail.com", "Very good", "Wack af", "Good evening")]
+    component.feedbacks = [new Feedback("John", "john@example.com", "Steve", "steve@example.com", "Very good", "Wack af", "Good evening")]
     component.type = FeedbackType.Received
     fixture.detectChanges();
     expect(fixture.nativeElement.querySelector("span").textContent).toContain('From:');
