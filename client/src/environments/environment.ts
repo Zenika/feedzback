e8137d4314904adcb243d573f6ba82dbe93dcb42
bnyat22 <bnyat.azizsharif@zenika.com>
2022-05-30 10:11:11 +0200

    environment key.json

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
   serverApi: 'http://localhost:4000/graphql',
-  firebaseConfig: {
-    apiKey: "AIzaSyAKtg1emw7hq7teSDzrhMXmh6uFWC4lDAc",
-    authDomain: "feedzback-343709.firebaseapp.com",
-    projectId: "feedzback-343709",
-    storageBucket: "feedzback-343709.appspot.com",
-    messagingSenderId: "370604731143",
-    appId: "1:370604731143:web:316617cb05f1a3611533a2",
-    measurementId: "G-HDCC6605DV"
-  }
+  firebaseConfig: '<Firebase_Config>'
 };
 
 /*
