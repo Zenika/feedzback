137fb7db020bbfd1a1b995d09d1f100ed1f70be9
bnyat22 <bnyat.azizsharif@zenika.com>
2022-05-31 15:51:14 +0200

    firebase account service key set into circleci

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ import {resolvers} from './src/graphql/resolvers.js';
 import cors from 'cors';
 import admin from 'firebase-admin'
 import {createRequire} from 'module'
+import dotEnv from 'dotenv'
 
 const server = new ApolloServer({typeDefs, resolvers, introspection: true,
   playground: true});
 const app = express();
 
 const require = createRequire(import.meta.url);
-const serviceFirebaseAccountKey = require('./serviceFirebaseAccountKey.json')
+if (process.env.NODE_ENV !== 'production') {
+  dotEnv.config()
+}
+const serviceFirebaseAccountKey = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceFirebaseAccountKey)
 })
