322e1226ca8cb3daffa8cbdabdd5a5070d9c16ae
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-07 15:08:10 +0200

    add test unit in main

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -32,12 +32,12 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app).toBeTruthy();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'FeedZback'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app.title).toEqual('FeedZback');
+    expect(app.title).toEqual('FeedZback');
   });
 });
