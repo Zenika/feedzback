1754722bbd8e5dfe9235e86f2485abe2f4bd2240
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-08 16:58:51 +0200

    add env global in client

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,14 +1,13 @@
 
 import { defineConfig } from "cypress";
 import admin = require("firebase-admin");
+require('dotenv').config();
 const cypressFirebasePlugin = require("cypress-firebase").plugin;
 export default defineConfig({
   e2e: {
     baseUrl : 'http://localhost:4200',
     setupNodeEvents(on, config) {
-      console.log(config.env['hey']);
       admin.initializeApp({
-        
         credential: admin.credential.cert(config.env['FIREBASE_SERVICE_KEY'])
       })
       
