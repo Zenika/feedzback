2d22539b9a2910609464c574b1ca090797a83bfb
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-09 14:24:38 +0200

    add firebase service account key

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,14 +1,14 @@
 
 import { defineConfig } from "cypress";
-import admin = require("firebase-admin");
-require('dotenv').config();
+import admin from "firebase-admin";
+const firebaseKey = require('firebase-service-key.json');
 const cypressFirebasePlugin = require("cypress-firebase").plugin;
 export default defineConfig({
   e2e: {
     baseUrl : 'http://localhost:4200',
     setupNodeEvents(on, config) {
       admin.initializeApp({
-        credential: admin.credential.cert(config.env['FIREBASE_SERVICE_KEY'])
+        credential: admin.credential.cert(firebaseKey)
       })
       
       cypressFirebasePlugin(on , config ,admin)
