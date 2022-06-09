37bbcffb8f170c876836d8425f6579bea883fe3e
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-09 16:26:56 +0200

    set firebase config in prod env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  serverApi: '<serverApi>'
+  serverApi: '<serverApi>',
+  firebaseConfig: {
+    apiKey: "AIzaSyAKtg1emw7hq7teSDzrhMXmh6uFWC4lDAc",
+    authDomain: "feedzback-343709.firebaseapp.com",
+    projectId: "feedzback-343709",
+    storageBucket: "feedzback-343709.appspot.com",
+    messagingSenderId: "370604731143",
+    appId: "1:370604731143:web:316617cb05f1a3611533a2",
+    measurementId: "G-HDCC6605DV"
+  }
 };
