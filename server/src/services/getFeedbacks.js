f25754261c8132c6f6b648340c20b5b17d4c46e1
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-22 17:26:34 +0200

    add getFeedbackById service

--- server/src/services/getFeedbacks.js
+++ server/src/services/getFeedbacks.js
@@ -8,7 +8,8 @@ export const getReceivedFeedbacks = async (email) => {
   try {
     const query = datastore.createQuery('feedzback').filter('receverEmail', '=', email);
     const [entities] = await datastore.runQuery(query);
-    return entities;
+    const res = combineEntityAndKey(entities);
+    return res
   } catch (err) {
     return err;
   }
@@ -17,8 +18,9 @@ export const getReceivedFeedbacks = async (email) => {
 export const getSentFeedbacks = async (email) => {
   try {
     const query = datastore.createQuery('feedzback').filter('senderEmail', '=', email);
-    const [entities] = await datastore.runQuery(query);
-    return entities;
+    let [entities] = await datastore.runQuery(query);
+    const res = combineEntityAndKey(entities);
+    return res;
   } catch (err) {
     return err;
   }
@@ -28,10 +30,23 @@ export const getSentFeedbacks = async (email) => {
 
 export const getFeedbackById = async (id) => {
   try {
-    const query = datastore.createQuery('feedzback').filter('NOM/ID', '=', id);
-    const [entities] = await datastore.runQuery(query);
-    return entities;
+    const key = datastore.key(['feedzback', parseInt(id,10)])
+    const query = datastore.createQuery('feedzback').filter('__key__', '=', key);
+    const [[entity]] = await datastore.runQuery(query);
+    return entity;
   } catch (err) {
     return err;
   }
 };
+
+export const combineEntityAndKey = (entities) => {
+   let list = [];
+   for(let i=0;i<entities.length;i++)
+   {
+    const res = Object.assign(entities[i],entities[i][datastore.KEY])
+    list.push(res)
+   }
+   return list;
+
+}
+
