b4e158b10fa9919b46976864ea65745c45b59621
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-23 11:35:22 +0200

    clean up

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { HomeComponent } from './home/home.component';
 import { SendAskFeedbackResultComponent } from './send-ask-feedback-result/send-ask-feedback-result.component';
 import { SendFeedbackFormComponent } from './send-feedback-form/send-feedback-form.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+import { redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
 const redirectUnauthorizedToSignInPage = () => redirectUnauthorizedTo(['sign-in']);
 import { MyFeedbacksPageComponent } from './my-feedbacks-page/my-feedbacks-page.component';
 import { MasterAuthGuard } from './services/auth-guard/master-auth.guard';
+import { FeedbackComponent } from './feedback/feedback.component';
 const routes: Routes = [  
   {path:'home', component:HomeComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
   {path:'ask', component:AskFeedbackFormComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
   {path:'send', component:SendFeedbackFormComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
   {path:'feedbacks', component:MyFeedbacksPageComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
   {path:'feedback', component:FeedbackComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
+  {path:'result', component:SendAskFeedbackResultComponent, canActivate: [MasterAuthGuard], data: { authGuardPipe: redirectUnauthorizedToSignInPage }},
   {path:'sign-in', component:SignInComponent},
   {path:'**',redirectTo:'sign-in'}
 ];
