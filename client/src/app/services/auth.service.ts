8c401d801fc1a3449ab65a3f9171387d25448ca6
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-27 16:23:51 +0200

    remove api

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -29,8 +29,8 @@ export class AuthService {
     console.log('loaded client')
   
   // await  gapi.client.init({
-  //     apiKey: 'AIzaSyAKtg1emw7hq7teSDzrhMXmh6uFWC4lDAc',
-  //      clientId: '370604731143-th76hjjdiag2vftad9ldvkcbh6ag51qq.apps.googleusercontent.com',
+  //     apiKey: '',
+  //      clientId: '',
   //     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
   //     plugin_name: "chat",
   //      scope: 'https://www.googleapis.com/auth/contacts.other.readonly'
