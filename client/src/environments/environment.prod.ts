efd8ab4505efd6288af1a7ff2793b05217e50dd3
bnyat22 <bnyat.azizsharif@zenika.com>
2022-06-28 16:00:11 +0200

    little improvements

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverApi: '<serverApi>',
+  serverApi: '<serverApi>/graphql',
   firebaseConfig: {
     apiKey: "AIzaSyAKtg1emw7hq7teSDzrhMXmh6uFWC4lDAc",
     authDomain: "feedzback-343709.firebaseapp.com",
