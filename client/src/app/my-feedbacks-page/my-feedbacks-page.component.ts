d5a1b11bfe7b6382cc5430558e75e5a935ee70f0
bnyat22 <bnyat.azizsharif@zenika.com>
2022-07-19 13:07:38 +0200

    add watch query in feedback list

--- client/src/app/my-feedbacks-page/my-feedbacks-page.component.ts
+++ client/src/app/my-feedbacks-page/my-feedbacks-page.component.ts
@@ -45,18 +45,19 @@ export class MyFeedbacksPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.apollo.query({
+    this.apollo.watchQuery({
       query: this.query,
       variables: {
         email: this.email
-      }
-    }).pipe(
+      },
+      pollInterval: 500
+    
+    }).valueChanges.pipe(
       map(({ data }) => data),
       takeUntil(this.unsubscribe$)
     ).subscribe(({ receivedFeedbacks, sentFeedbacks }: any) => {
       this.receivedFeedbacks = receivedFeedbacks
       this.sentFeedbacks = sentFeedbacks
-      console.log(this.receivedFeedbacks)
     })
   }
 
