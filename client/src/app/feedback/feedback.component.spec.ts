b67b3e368da47e37853935ba723ce9e0118c1457
bnyat22 <bnyat.azizsharif@zenika.com>
2022-07-28 14:42:21 +0200

    some improvements

--- client/src/app/feedback/feedback.component.spec.ts
+++ client/src/app/feedback/feedback.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Apollo } from 'apollo-angular';
+import { FeedbackType } from '../enum/feedback-type';
 import { Feedback } from '../model/feedback';
 
 import { FeedbackComponent } from './feedback.component';
@@ -8,8 +10,8 @@ import { FeedbackComponent } from './feedback.component';
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
-  const feedback = new Feedback('1','11','Pierre','Pierre@exemple.com', 
-  'marie@example.com', 'marie', '...', '...', '...', '121212');
+  const feedback = new Feedback('1','11','Pierre','mierre@exemple.com', 
+  'marie@example.com', 'Marie', '...', '...', '...', '121212');
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,11 +26,16 @@ describe('FeedbackComponent', () => {
     fixture = TestBed.createComponent(FeedbackComponent);
     component = fixture.componentInstance;
     component.feedback = feedback;
+    component.type = 'Received'
     fixture.detectChanges();
   });
 
   it('should create', () => {
- 
     expect(component).toBeTruthy();
   });
+  it('should show sender email when the feedback type is receved', ()=> {
+    const receverEmail = fixture.debugElement.query(By.css('h4')).nativeElement.textContent
+    expect(receverEmail).toContain('marie@example.com')
+  })
+  
 });
