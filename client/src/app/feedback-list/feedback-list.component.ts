3be0b03e3a92fe7919d0bb1338434a517a46063d
bnyat22 <bnyat.azizsharif@zenika.com>
2022-08-03 16:54:25 +0200

    centrer le detail de feedback en mode mobile

--- client/src/app/feedback-list/feedback-list.component.ts
+++ client/src/app/feedback-list/feedback-list.component.ts
@@ -15,35 +15,17 @@ export class FeedbackListComponent implements OnInit {
   @Input() type!: FeedbackType;
   public feedbackType: typeof FeedbackType = FeedbackType;
   sortedFeedbackList!: Feedback[]
- 
-
-  constructor(private router:Router) { }
+  datePipe!: DatePipe
 
   ngOnInit(): void {
-    const arr1 = [
-      { id: 3, date: new Date('2022-04-21') },
-      { id: 5, date: new Date('2027-04-21') },
-      { id: 2, date: new Date('2023-01-21') },
-    ];
-    
-    // ✅ Sort in Ascending order (low to high)
-    const sortedAsc = arr1.sort(
-      (objA, objB) => {
-        console.log(objA.date.getTime() + ' and  ' + objB.date.getTime() )
-        console.log(objA.date.getTime() - objB.date.getTime())
-        return objA.date.getTime() - objB.date.getTime()
-      } 
-    );
-    console.log(sortedAsc);
+    this.datePipe = new DatePipe("en-US")
   }
   ngAfterContentChecked(): void {
-    //Called after every check of the component's or directive's content.
-    //Add 'implements AfterContentChecked' to the class.
-    const datePipe = new DatePipe("en-US")
-  this.sortedFeedbackList = [...this.feedbacks]
-
-     this.sortedFeedbackList.sort((a, b) => new Date(datePipe.transform(b.createdAt, 'yyyy-MM-dd')!).getTime() - new Date(datePipe.transform(a.createdAt, 'yyyy-MM-dd')!).getTime())
-   //  console.log(this.sortedFeedbackList)
-
+    this.sortFeedbackList()
+  }
+  sortFeedbackList() {
+    this.sortedFeedbackList = [...this.feedbacks]
+    this.sortedFeedbackList.sort((a, b) => new Date(this.datePipe.transform(b.createdAt, 'yyyy-MM-dd')!).
+    getTime() - new Date(this.datePipe.transform(a.createdAt, 'yyyy-MM-dd')!).getTime())
   }
 }
