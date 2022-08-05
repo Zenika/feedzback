7a63713f76aa19b4a2399597c7570058a51d3499
bnyat22 <bnyat.azizsharif@zenika.com>
2022-08-05 12:33:08 +0200

    little change

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverApi: process.env['SERVER_API'],
+  serverApi: 'SERVER',
   firebaseConfig: {
     apiKey: "AIzaSyAKtg1emw7hq7teSDzrhMXmh6uFWC4lDAc",
     authDomain: "feedzback-343709.firebaseapp.com",
