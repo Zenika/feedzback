f7a588a5c23ea6ea5b25336838996a96d31fc04e
Vincent Landais <vincent.landais@zenika.com>
2023-06-30 14:36:19 +0200

    dev firebase config added

---
+++ client/src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  serverApi: '<SERVER_API>/graphql',
+  firebaseConfig: {
+    apiKey: "AIzaSyDkkgGTo54Osh-rFa3kmpDaDY15xN8Ois0",
+    authDomain: "feedzbackdev-389312.firebaseapp.com",
+    projectId: "feedzbackdev-389312",
+    storageBucket: "feedzbackdev-389312.appspot.com",
+    messagingSenderId: "241997452771",
+    appId: "1:241997452771:web:f543b1756557362424bcd6",
+    measurementId: "G-7BX0QVS63J"
+  },
+};
