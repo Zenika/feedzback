d48425e5b3e9d81fa99897379a583336b03d813c
bnyat <binyat.sharif@gmail.com>
2023-07-28 15:33:24 +0200

    rebase main

--- client/src/app/my-ask-feedbacks-page/my-ask-feedbacks-page.component.spec.ts
+++ client/src/app/my-ask-feedbacks-page/my-ask-feedbacks-page.component.spec.ts
@@ -1,4 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Apollo } from 'apollo-angular';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { AskFeedbackListComponent } from '../ask-feedback-list/ask-feedback-list.component';
+import { AuthService } from '../services/auth.service';
+import { authStub } from '../services/authStub';
+import { TabLinkComponent } from '../tab-link/tab-link.component';
+import { TabsComponent } from '../tabs/tabs.component';
 
 import { MyAskFeedbacksPageComponent } from './my-ask-feedbacks-page.component';
 
@@ -11,6 +19,19 @@ describe('MyAskFeedbacksPageComponent', () => {
       declarations: [ MyAskFeedbacksPageComponent ]
     })
     .compileComponents();
+  });
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, ApolloTestingModule],
+      providers: [Apollo, {provide: AuthService, useValue: authStub}],
+      declarations: [
+        MyAskFeedbacksPageComponent,
+        TabsComponent,
+        TabLinkComponent,
+        AskFeedbackListComponent,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
