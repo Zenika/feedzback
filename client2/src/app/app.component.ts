13537ac73381ba64f8dd79b6796a78d58a09a3b3
Stéphane Francel <stephane.francel@gmail.com>
2023-10-26 21:08:35 +0200

    feat(client2): add eslint and prettier

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,8 +7,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent {}
