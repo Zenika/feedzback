34ea175687dd55529b8b462427a513ffa4fb508d
Stéphane Francel <stephane.francel@gmail.com>
2023-10-31 16:06:27 +0100

    feat(client2): Improve feedback type helper

--- client2/src/app/feedback-details/feedback-details.helpers.ts
+++ client2/src/app/feedback-details/feedback-details.helpers.ts
@@ -1,7 +1,7 @@
 import { FeedbackType } from './feedback-details.types';
 
-export const getFeedbackType = (value: string): FeedbackType | void => {
-  const _value = value.toLowerCase();
+export const getFeedbackType = (value: string | null | undefined): FeedbackType | void => {
+  const _value = value?.toLowerCase();
   if (_value === FeedbackType.received || _value === FeedbackType.sent) {
     return _value;
   }
