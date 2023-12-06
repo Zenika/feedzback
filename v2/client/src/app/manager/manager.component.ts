534dbace0a8ce4472c5f34c91637c65c30bdfb16
Stéphane Francel <stephane.francel@gmail.com>
2023-12-06 15:52:23 +0100

    v2(client): manager -> consultant as queryParam

--- v2/client/src/app/manager/manager.component.ts
+++ v2/client/src/app/manager/manager.component.ts
@@ -1,15 +1,16 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, HostBinding, ViewEncapsulation, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, HostBinding, Input, ViewEncapsulation, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { switchMap } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { BehaviorSubject, of, switchMap } from 'rxjs';
 import { FeedbackService } from '../shared/feedback/feedback.service';
 import { ManagerFeedbackListComponent } from './manager-feedback-list/manager-feedback-list.component';
 import { ManagerService } from './manager.service';
+import { ManagerData } from './manager.types';
 
 @Component({
   selector: 'app-manager',
@@ -17,6 +18,7 @@ import { ManagerService } from './manager.service';
   imports: [
     AsyncPipe,
     ReactiveFormsModule,
+    RouterLink,
     MatButtonModule,
     MatFormFieldModule,
     MatIconModule,
@@ -27,21 +29,38 @@ import { ManagerService } from './manager.service';
   styleUrl: './manager.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export default class ManagerComponent {
+export default class ManagerComponent implements ManagerData {
   @HostBinding('class.app-manager') hasCss = true;
 
-  protected managerService = inject(ManagerService);
+  protected consultant$ = new BehaviorSubject<string>('');
+
+  @Input() set consultant(value: string) {
+    this.consultant$.next(value);
+  }
+  get consultant() {
+    return this.consultant$.value;
+  }
+
+  protected router = inject(Router);
+
+  protected activatedRoute = inject(ActivatedRoute);
+
+  protected consultants = inject(ManagerService).consultants;
 
   protected feedbackService = inject(FeedbackService);
 
-  private formBuilder = inject(NonNullableFormBuilder);
-
-  protected form = this.formBuilder.group({
-    consultant: [],
-  });
-
-  protected feedbacks$ = this.form.controls.consultant.valueChanges.pipe(
-    takeUntilDestroyed(),
-    switchMap(() => this.feedbackService.getManagerConsultantFeedbacks(this.form.controls.consultant.value)),
+  protected feedbacks$ = this.consultant$.pipe(
+    switchMap((consultant) => {
+      if (this.consultants.includes(consultant)) {
+        return this.feedbackService.getManagerConsultantFeedbacks(consultant);
+      } else {
+        return of([]);
+      }
+    }),
   );
+
+  protected consultantChange(consultant: string) {
+    const queryParams: ManagerData = { consultant };
+    this.router.navigate([], { relativeTo: this.activatedRoute, queryParams });
+  }
 }
