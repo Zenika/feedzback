8f83ab9b47453573925bfa4bc608c3d4cf28ca85
Stéphane Francel <stephane.francel@gmail.com>
2023-12-08 09:51:00 +0100

    v2(client): Minor CSS changes

--- v2/client/src/app/manager/manager-feedback-list/manager-feedback-list.animations.ts
+++ v2/client/src/app/manager/manager-feedback-list/manager-feedback-list.animations.ts
@@ -4,6 +4,6 @@ export const managerFeedbackListAnimations = [
   trigger('detailExpand', [
     state('collapsed,void', style({ height: '0px', minHeight: '0' })),
     state('expanded', style({ height: '*' })),
-    transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    transition('expanded <=> collapsed', animate('225ms ease')),
   ]),
 ];
