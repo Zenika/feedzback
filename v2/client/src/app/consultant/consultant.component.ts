3c01387569b21e87bcf97ffcefec56f4fc8ffb06
Stéphane Francel <stephane.francel@gmail.com>
2023-12-18 14:24:16 +0100

    v2(client): Improve Consultant component

--- v2/client/src/app/consultant/consultant.component.ts
+++ v2/client/src/app/consultant/consultant.component.ts
@@ -1,11 +1,13 @@
 import { Component, HostBinding, ViewEncapsulation, inject } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { ConsultantService } from '../shared/consultant/consultant.service';
 import { ValidationErrorMessagePipe } from '../shared/form/validation-error-message';
+import { MessageComponent } from '../shared/ui/message/message.component';
+import { ALLOWED_EMAIL_DOMAINS, allowedEmailDomainsValidatorFactory } from '../shared/form/allowed-email-domains';
 
 @Component({
   selector: 'app-consultant',
@@ -17,6 +19,7 @@ import { ValidationErrorMessagePipe } from '../shared/form/validation-error-mess
     MatIconModule,
     MatInputModule,
     ValidationErrorMessagePipe,
+    MessageComponent,
   ],
   templateUrl: './consultant.component.html',
   styleUrl: './consultant.component.scss',
@@ -29,17 +32,34 @@ export class ConsultantComponent {
 
   protected submitInProgress = false;
 
+  private currentManagerEmail = this.consultantService.data().managerEmail;
+
+  private allowedEmailDomainsValidator = allowedEmailDomainsValidatorFactory(inject(ALLOWED_EMAIL_DOMAINS));
+
   protected form = inject(NonNullableFormBuilder).group({
-    managerEmail: [this.consultantService.data().managerEmail],
+    managerEmail: [this.currentManagerEmail, [Validators.email, this.allowedEmailDomainsValidator]],
   });
 
-  protected managedEmails: string[] = [];
+  protected get submitButtonDisabled() {
+    return this.form.value.managerEmail === this.currentManagerEmail || this.form.invalid || this.submitInProgress;
+  }
+
+  protected success: boolean | null = null;
 
   protected onSubmit() {
+    this.form.disable();
     this.submitInProgress = true;
     this.consultantService.updateManager(this.form.value.managerEmail!).subscribe({
-      error: () => (this.submitInProgress = false),
-      complete: () => (this.submitInProgress = false),
+      error: () => {
+        this.form.enable();
+        this.submitInProgress = false;
+        this.success = false;
+      },
+      complete: () => {
+        this.form.enable();
+        this.submitInProgress = false;
+        this.success = true;
+      },
     });
   }
 }
