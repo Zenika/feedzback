66bc3f9a03377e662007eb437267b7154fb88d4a
Stéphane Francel <stephane.francel@gmail.com>
2023-12-22 00:36:05 +0100

    server: refactoring cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptionsDelegate } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Request } from 'express';
 import { AppModule } from './app.module';
 import { AppConfig } from './core/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: function corsOptionsDelegate(_, corsOptionsCallback) {
-      // Note: the function `corsOptionsDelegate` is executed asynchronously so that `configService` is well defined
-      const origin = configService.get('clientUrl');
-      corsOptionsCallback(null as any, { origin });
-    },
-  });
+  // Note:
+  //  This function is executed asynchronously so that `configService` is well defined
+  // More infos:
+  //  - https://docs.nestjs.com/security/cors
+  //  - https://github.com/expressjs/cors#configuration-options
+  const cors: CorsOptionsDelegate<Request> = (req, corsOptionsCallback) => {
+    const origin = configService.get('clientUrl');
+    corsOptionsCallback(null as any, { origin });
+  };
+
+  const app = await NestFactory.create(AppModule, { cors });
 
   app.useGlobalPipes(new ValidationPipe());
 
