c88068c724f518f7a8d56d2e2eb9a6ea0e24c701
Stéphane Francel <stephane.francel@gmail.com>
2023-12-28 18:19:54 +0100

    client: update environment to use firebase prod

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,13 +1,13 @@
 import { FirebaseOptions } from 'firebase/app';
 
 const firebaseOptions: FirebaseOptions = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: 'AIzaSyBJuOG9cIfUN1s1d6uYvB2eo1zKHDFXKZY',
+  authDomain: 'feedzback-v2.firebaseapp.com',
+  projectId: 'feedzback-v2',
+  storageBucket: 'feedzback-v2.appspot.com',
+  messagingSenderId: '690479787965',
+  appId: '1:690479787965:web:feeb973edf7ec5499e6cd4',
+  measurementId: 'G-V2T17HJ69J',
 };
 
 export const environment = {
