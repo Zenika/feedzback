33daf31588871af76364fc2c307b9f5b9a95101a
Pierre Nicoli <pierre.nicoli@zenika.com>
2023-12-29 16:49:58 +0100

    Adding production definition (partial for client)

---
+++ client/src/environments/environment.production.ts
@@ -0,0 +1,21 @@
+import { FirebaseOptions } from 'firebase/app';
+
+const firebaseOptions: FirebaseOptions = {
+  apiKey: "AIzaSyBJuOG9cIfUN1s1d6uYvB2eo1zKHDFXKZY",
+  authDomain: "feedzback-v2.firebaseapp.com",
+  projectId: "feedzback-v2",
+  storageBucket: "feedzback-v2.appspot.com",
+  messagingSenderId: "690479787965",
+  appId: "1:690479787965:web:feeb973edf7ec5499e6cd4",
+  measurementId: "G-V2T17HJ69J"
+};
+
+export const environment = {
+  production: true,
+  firebaseOptions,
+  apiBaseUrl: 'https://feedzback-staging-v54ioxu74a-ew.a.run.app',
+  allowedEmailDomains: ['zenika.com'],
+  featureFlipping: {
+    manager: false,
+  },
+};
