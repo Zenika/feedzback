fb6e451af161e116d6bd6dc6b71665c435c50b8f
Stéphane Francel <stephane.francel@gmail.com>
2024-01-02 15:04:48 +0100

    Temporary: Check server request

--- server/src/health/health.controller.ts
+++ server/src/health/health.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import safeStringify from 'fast-safe-stringify';
 
 @Controller('health')
 export class HealthController {
@@ -7,5 +9,23 @@ export class HealthController {
   @Get('')
   async check() {
     return { ok: 'I feel good.' };
+  }
+
+  @Get('req')
+  req(@Req() req: Request) {
+    return `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>FeedZback</title>
+    <base href="/" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <pre>Protocol: ${safeStringify(req.protocol, undefined, 2)}</pre>
+    <pre>Headers ${safeStringify(req.headers, undefined, 2)}</pre>
+    <pre>Request ${safeStringify(req, undefined, 2)}</pre>
+  </body>
+</html>`;
   }
 }
