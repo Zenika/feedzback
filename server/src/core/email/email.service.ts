912b24a19a1baf3459363ed41d533fac36b9d288
Stéphane Francel <stephane.francel@gmail.com>
2024-01-05 15:15:46 +0100

    client: translate (in progress...)

--- server/src/core/email/email.service.ts
+++ server/src/core/email/email.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as FormData from 'form-data';
+import FormData from 'form-data';
 import Mailgun, { MailgunMessageData } from 'mailgun.js';
 import { AppConfig } from '../config';
 
