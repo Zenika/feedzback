a7b33f916400a5249443cceef3923c84cb10d291
Stéphane Francel <stephane.francel@gmail.com>
2024-01-06 23:57:19 +0100

    servier: minor refactoring

--- server/src/core/email/email.service.ts
+++ server/src/core/email/email.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import FormData from 'form-data';
+import * as FormData from 'form-data';
 import Mailgun, { MailgunMessageData } from 'mailgun.js';
 import { AppConfig } from '../config';
 
 @Injectable()
 export class EmailService {
-  private mailgunOptions = this.configService.get('mailgunClientOptions', { infer: true })!;
+  private clientOptions = this.configService.get('mailgunClientOptions', { infer: true })!;
 
-  private mailgunDomain = this.configService.get('mailgunDomain', { infer: true })!;
+  private client = new Mailgun(FormData).client(this.clientOptions);
 
-  private mailgunClient = new Mailgun(FormData).client(this.mailgunOptions);
+  private domain = this.configService.get('mailgunDomain', { infer: true })!;
 
   constructor(private configService: ConfigService<AppConfig>) {}
 
   async send({ from, to, subject, html }: Required<Pick<MailgunMessageData, 'from' | 'to' | 'subject' | 'html'>>) {
-    await this.mailgunClient.messages.create(this.mailgunDomain, { from, to, subject, html });
+    await this.client.messages.create(this.domain, { from, to, subject, html });
   }
 }
