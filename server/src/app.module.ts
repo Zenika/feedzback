b5104dbdcf3181434ec6b995f02589f863a938fc
Stéphane Francel <stephane.francel@gmail.com>
2024-01-12 11:07:01 +0100

    server: fix middleware incompatibilities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,6 +7,7 @@ import { CryptoModule } from './core/crypto/crypto.module';
 import { EmployeeModule } from './employee';
 import { FeedbackModule } from './feedback';
 import { HealthModule } from './health';
+import { PeopleMiddleware } from './people/people.middleware';
 import { PeopleModule } from './people/people.module';
 
 @Module({
@@ -25,6 +26,11 @@ import { PeopleModule } from './people/people.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(ContextMiddleware).forRoutes('');
-    consumer.apply(AuthMiddleware).forRoutes('');
+
+    // Warning: `AuthMiddleware` and `PeopleMiddleware` are incompatible and cannot be used on the same routes.
+    // They both use the `Authorization` header of the request to check the Bearer token,
+    // but `AuthMiddleware` expects an `idToken` while `PeopleMiddleware` expects an `accessToken`.
+    consumer.apply(AuthMiddleware).forRoutes('feedback', 'employee');
+    consumer.apply(PeopleMiddleware).forRoutes('people');
   }
 }
--- server/src/people/people.module.ts
+++ server/src/people/people.module.ts
@@ -1,14 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PeopleController } from './people.controller';
-import { PeopleMiddleware } from './people.middleware';
 import { PeopleService } from './people.service';
 
 @Module({
   controllers: [PeopleController],
   providers: [PeopleService],
+  exports: [PeopleService],
 })
-export class PeopleModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(PeopleMiddleware).forRoutes('people');
-  }
-}
+export class PeopleModule {}
