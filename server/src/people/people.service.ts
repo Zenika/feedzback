c45b14145428625b8c2351e1e7972ca6c04352ed
NOrbert <62135994+NorbertPointu@users.noreply.github.com>
2024-01-16 18:11:19 +0100

    remove googleapis package

--- server/src/people/people.service.ts
+++ server/src/people/people.service.ts
@@ -11,7 +11,8 @@ export class PeopleService {
   async searchDirectoryPeople(query: string) {
     const response = await people('v1').people.searchDirectoryPeople({
       access_token: this.accessToken,
-      sources: ['DIRECTORY_SOURCE_TYPE_DOMAIN_PROFILE'],
+      sources: ['DIRECTORY_SOURCE_TYPE_DOMAIN_PROFILE', 'DIRECTORY_SOURCE_TYPE_DOMAIN_CONTACT'],
+      mergeSources: ['DIRECTORY_MERGE_SOURCE_TYPE_CONTACT'],
       readMask: 'emailAddresses,names,photos',
       query,
     });
