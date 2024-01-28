857349dbad4a33c6ac6c96f76f5256f64d8d3e18
Stéphane Francel <stephane.francel@gmail.com>
2024-01-29 00:26:52 +0100

    Client: update packages - Fix Jest setup to work with i18n

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,5 +1,8 @@
 // Jest builder options (https://www.npmjs.com/package/@angular-builders/jest#builder-options)
 
-module.exports = {
+import type { Config } from 'jest';
+
+export default {
   passWithNoTests: true,
-};
+  setupFiles: ['./setup-jest.ts'],
+} satisfies Config;
