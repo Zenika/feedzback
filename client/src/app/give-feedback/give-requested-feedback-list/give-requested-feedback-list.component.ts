a419206e869caff404e8c756d8433efc69a57d5b
Stéphane Francel <stephane.francel@gmail.com>
2024-02-01 15:27:28 +0100

    Fix and improve wording and translations

--- client/src/app/give-feedback/give-requested-feedback-list/give-requested-feedback-list.component.ts
+++ client/src/app/give-feedback/give-requested-feedback-list/give-requested-feedback-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, HostBinding, ViewEncapsulation, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 import { map } from 'rxjs';
 import { normalizeReceivedRequestList } from '../../my-feedbacks/my-feedbacks.utils';
 import { FeedbackListComponent } from '../../shared/feedback/feedback-list/feedback-list.component';
 import { FeedbackService } from '../../shared/feedback/feedback.service';
+import { MessageComponent } from '../../shared/ui/message/message.component';
 
 @Component({
   selector: 'app-give-requested-feedback-list',
   standalone: true,
-  imports: [MatIconModule, FeedbackListComponent],
+  imports: [RouterLink, MatIconModule, FeedbackListComponent, MessageComponent],
   templateUrl: './give-requested-feedback-list.component.html',
   styleUrl: './give-requested-feedback-list.component.scss',
   encapsulation: ViewEncapsulation.None,
