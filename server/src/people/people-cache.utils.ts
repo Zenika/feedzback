15867cd3f5d5bdd6089216af002e01f1160b6067
Stéphane Francel <stephane.francel@gmail.com>
2024-02-07 16:09:51 +0100

    Refactoring

--- server/src/people/people-cache.utils.ts
+++ server/src/people/people-cache.utils.ts
@@ -3,29 +3,33 @@ import { SearchablePerson } from './people-cache.types';
 
 const consolidateSearchTokens = (rawSearchTokens: string[]): string[] =>
   rawSearchTokens
+    // Remove duplicate tokens
     .reduce((searchTokens, searchToken) => {
       if (!searchTokens.includes(searchToken)) {
         searchTokens.push(searchToken);
       }
       return searchTokens;
     }, [] as string[])
+    // Sort tokens by length
     .sort((a, b) => (a.length < b.length ? -1 : a.length > b.length ? 1 : 0));
 
 const buildSearchTokens = (value?: string): string[] =>
   (value ?? '')
     .toLowerCase()
-    .split(/\.|-|_|\s+/g)
+    .split(/\s+/g)
+    .map((searchToken) => {
+      if (searchToken.includes('@')) {
+        const [username] = searchToken.split('@');
+        return [searchToken, ...username.split(/\.|-|_/g)];
+      }
+      return searchToken.split(/\.|-|_/g);
+    })
+    .flat()
     .filter((searchToken) => searchToken !== '');
-
-const buildEmailSearchTokens = (email: string): string[] => {
-  const emailLowerCase = email.trim().toLowerCase();
-  const [username] = emailLowerCase.split('@');
-  return [emailLowerCase, ...buildSearchTokens(username)];
-};
 
 export const mapToSearchablePerson = (person: Person): SearchablePerson => {
   const searchTokens = consolidateSearchTokens([
-    ...buildEmailSearchTokens(person.email),
+    ...buildSearchTokens(person.email),
     ...buildSearchTokens(person.displayName),
   ]);
   return { ...person, searchTokens };
@@ -33,7 +37,6 @@ export const mapToSearchablePerson = (person: Person): SearchablePerson => {
 
 export const searchPersons = (query: string, searchablePersons: SearchablePerson[]): Person[] => {
   const querySearchTokens = buildSearchTokens(query);
-
   return searchablePersons
     .filter(({ searchTokens }) =>
       querySearchTokens.every((querySearchToken) =>
