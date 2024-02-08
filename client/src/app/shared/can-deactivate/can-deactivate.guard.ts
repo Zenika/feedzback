de4a91a92e492cf24aa71aa4df3f3dd7d693f54b
NOrbert <62135994+NorbertPointu@users.noreply.github.com>
2024-02-08 15:36:02 +0100

    remove CanDeactivate guard

--- client/src/app/shared/can-deactivate/can-deactivate.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { CanDeactivateForm } from './can-deactivate.type';
-
-export const canDeactivateGuard: CanDeactivateFn<CanDeactivateForm> = (component, currentRoute, currentState, nextState) => {
-  return component.canDeactivate();
-};
