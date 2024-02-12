988aa31616e52a2eeae28006de1e931a17a5b113
Pierre-Yves Aillet <pyaillet@gmail.com>
2024-02-12 15:33:50 +0100

    :alembic: testing with env dev-remote

---
+++ client/src/environments/environment.dev-remote.ts
@@ -0,0 +1,24 @@
+import { FirebaseOptions } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY-HULaNWkEPCoQWCIy3vOB2KB98z4HGs",
+  authDomain: "feedzback-v2-dev.firebaseapp.com",
+  projectId: "feedzback-v2-dev",
+  storageBucket: "feedzback-v2-dev.appspot.com",
+  messagingSenderId: "195593920321",
+  appId: "1:195593920321:web:e9d6255b8ef9a46466cbc0",
+  measurementId: "G-29CT4FJL84"
+};
+
+export const environment = {
+  production: false,
+  firebaseOptions,
+  apiBaseUrl: 'https://server.dev.feedzback.znk.io',
+  allowedEmailDomains: ['zenika.com', 'zenika.ch'],
+  featureFlipping: {
+    manager: true,
+    localize: false,
+    requestTemplate: true,
+    autocompleteEmail: true,
+  },
+};
