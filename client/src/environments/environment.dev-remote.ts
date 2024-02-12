7ac10f9705158f358b3fa4c2f89a5b54e68a6195
Pierre-Yves Aillet <pyaillet@gmail.com>
2024-02-12 15:45:07 +0100

    :alembic: fix linter errors

--- client/src/environments/environment.dev-remote.ts
+++ client/src/environments/environment.dev-remote.ts
@@ -1,13 +1,13 @@
 import { FirebaseOptions } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBY-HULaNWkEPCoQWCIy3vOB2KB98z4HGs",
-  authDomain: "feedzback-v2-dev.firebaseapp.com",
-  projectId: "feedzback-v2-dev",
-  storageBucket: "feedzback-v2-dev.appspot.com",
-  messagingSenderId: "195593920321",
-  appId: "1:195593920321:web:e9d6255b8ef9a46466cbc0",
-  measurementId: "G-29CT4FJL84"
+const firebaseOptions: FirebaseOptions = {
+  apiKey: 'AIzaSyBY-HULaNWkEPCoQWCIy3vOB2KB98z4HGs',
+  authDomain: 'feedzback-v2-dev.firebaseapp.com',
+  projectId: 'feedzback-v2-dev',
+  storageBucket: 'feedzback-v2-dev.appspot.com',
+  messagingSenderId: '195593920321',
+  appId: '1:195593920321:web:e9d6255b8ef9a46466cbc0',
+  measurementId: 'G-29CT4FJL84',
 };
 
 export const environment = {
