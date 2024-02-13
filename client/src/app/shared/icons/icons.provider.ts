9c6b2a8cf56b3d3f874e02fb4e6c80499e30ef09
Stéphane Francel <stephane.francel@gmail.com>
2024-02-13 16:21:39 +0100

    fix(client): material-symbols package size

--- client/src/app/shared/icons/icons.provider.ts
+++ client/src/app/shared/icons/icons.provider.ts
@@ -7,7 +7,7 @@ import { svgIconNames } from './icons.constants';
 export const provideSvgIcons = (): FactoryProvider => ({
   provide: APP_INITIALIZER,
   useFactory: (iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, baseHref: string) => (): Promise<void> => {
-    // Set font class according to the NPM package installed: "material-symbols"
+    // Set font class according to the NPM package installed: "@material-symbols/font-600"
     // Values: 'material-symbols-outlined', 'material-symbols-rounded' or 'material-symbols-sharp'.
     // For more infos: https://fonts.google.com/icons
     iconRegistry.setDefaultFontSetClass('material-symbols-rounded');
