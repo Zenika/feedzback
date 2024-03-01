ea0f9befee0d4afa2f0d936e2c662477829c16a5
Stéphane Francel <stephane.francel@zenika.com>
2024-03-01 15:20:45 +0100

    feat(client): improve modal wording (#407)

--- client/src/app/shared/base-href/base-href.provider.ts
+++ client/src/app/shared/base-href/base-href.provider.ts
@@ -1,6 +1,7 @@
 import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
+import { FactoryProvider } from '@angular/core';
 
-export const provideBaseHref = () => ({
+export const provideBaseHref = (): FactoryProvider => ({
   provide: APP_BASE_HREF,
   useFactory: (platformLocation: PlatformLocation): string => platformLocation.getBaseHrefFromDOM(),
   deps: [PlatformLocation],
