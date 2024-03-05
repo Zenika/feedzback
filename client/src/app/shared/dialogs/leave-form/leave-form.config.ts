2a1fcfdc91d09068ec49eaa960c42ee4b939fd91
Stéphane Francel <stephane.francel@gmail.com>
2024-03-06 00:27:31 +0100

    fix(client): typo

--- client/src/app/shared/dialogs/leave-form/leave-form.config.ts
+++ client/src/app/shared/dialogs/leave-form/leave-form.config.ts
@@ -4,13 +4,13 @@ import { LeaveFormType } from './leave-form.types';
 export const leaveFormMap: Record<LeaveFormType, DialogData> = {
   quitFeedback: {
     title: $localize`:@@Component.LeaveForm.QuitFeedbackTitle:Quitter la page ?`,
-    content: $localize`:@@Component.LeaveForm.QuitFeedbackContent:Si vous quittez la page, le feedZback que vous avez commencé à remplir sera perdu.`,
+    content: $localize`:@@Component.LeaveForm.QuitFeedbackContent:Si vous quittez la page, le feedZback que vous avez commencé à rédiger sera perdu.`,
     confirm: { label: $localize`:@@Action.Quit:Quitter`, icon: 'alt_route' },
   },
 
   applyFeedbackDraft: {
     title: $localize`:@@Component.LeaveForm.ApplyFeedbackDraftTitle:Appliquer le brouillon ?`,
-    content: $localize`:@@Component.LeaveForm.ApplyFeedbackDraftContent:Si vous appliquez le brouillon, le feedZback que vous avez commencé à remplir sera perdu.`,
+    content: $localize`:@@Component.LeaveForm.ApplyFeedbackDraftContent:Si vous appliquez le brouillon, le feedZback que vous avez commencé à rédiger sera perdu.`,
     confirm: { label: $localize`:@@Action.Apply:Appliquer`, icon: 'edit_note' },
   },
 };
