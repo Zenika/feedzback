57ebb3727b3e9feaa2d415a5c197bf740ff53ab3
Stéphane Francel <stephane.francel@zenika.com>
2024-03-30 23:29:03 +0100

    feat(client): bypass the loading spinner for the "/employee" endpoint

--- client/src/app/shared/loading/loading.interceptor.ts
+++ client/src/app/shared/loading/loading.interceptor.ts
@@ -2,10 +2,11 @@ import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Observable, finalize } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { BYPASS_LOADING } from './loading.config';
 import { LoadingService } from './loading.service';
 
 export const loadingInterceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
-  if (!req.url.startsWith(environment.apiBaseUrl)) {
+  if (!req.url.startsWith(environment.apiBaseUrl) || req.context.get(BYPASS_LOADING)) {
     return next(req);
   }
   const loadingService = inject(LoadingService);
