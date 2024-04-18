cb446a9bb1a006400f71b59b32e6cc338d7d4dc0
Yasmine <yasmine@MacBook-Pro-de-Yasmine.local>
2024-04-18 16:28:12 +0200

    change sql request of age feedbacks requested no replied

--- usage-analytics/create-analytics/feedbacks_requests_age_repartition.sql
+++ usage-analytics/create-analytics/feedbacks_requests_age_repartition.sql
@@ -1,14 +1,16 @@
 WITH requests_feedbacks AS (
-    SELECT DATE_TRUNC(TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(DATA, "$.createdAt") AS INT)), DAY) AS day
+    SELECT DATE_TRUNC(TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(DATA, "$.createdAt") AS INT)), DAY) AS creation_day
     FROM `<PROJECT_NAME>.firestore_export.feedback_raw_latest`
-    WHERE   JSON_EXTRACT_SCALAR(DATA, "$.status") = "pending"
+    WHERE   JSON_EXTRACT_SCALAR(DATA, "$.status") = "pending" and JSON_EXTRACT_SCALAR(DATA, "$.archived")= "0"
 )
 
-SELECT count(*) feedbacks_no_replied,
+SELECT count(*) as nb_feedbacks,creation_day,
 CASE
-    WHEN date_diff(CURRENT_TIMESTAMP(),day,DAY) < 5 THEN "Requests less than 5 days old"
-    WHEN date_diff(CURRENT_TIMESTAMP(),day,DAY)>=5 and date_diff(CURRENT_TIMESTAMP(),day,DAY)<10 THEN "Requests between 5 and 10 days old"
-    WHEN date_diff(CURRENT_TIMESTAMP(),day,DAY)>=10 THEN "Requests older than 10 days"
-END AS period
+    WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=180 THEN "Requests older than 6 months "
+    WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=90 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<180 THEN "Requests between 3 months and 6 months old"
+    WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=30 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<90 THEN "Requests between one month and 3 months old"
+    WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=14 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<30 THEN "Requests between 2 weeks and one month old"
+    WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=0 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<14 THEN "Requests between 0 and 2 weeks old"
+END AS age
 from requests_feedbacks
-group by period
+group by creation_day
