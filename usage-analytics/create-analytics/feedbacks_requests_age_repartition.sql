a51dd4a2b1110b312227386e4f9dbe8f4afedfb5
yasmineDoghri6 <58326073+yasmineDoghri6@users.noreply.github.com>
2024-04-19 15:38:28 +0200

    Feedback no replied (#522)
    Delete <PROJECT_NAME> and replace it with the correct name for  feedbacks_requests_age_repartition request
    ---------
    
    Co-authored-by: Yasmine <yasmine@MacBook-Pro-de-Yasmine.local>

--- usage-analytics/create-analytics/feedbacks_requests_age_repartition.sql
+++ usage-analytics/create-analytics/feedbacks_requests_age_repartition.sql
@@ -1,6 +1,6 @@
 WITH requests_feedbacks AS (
     SELECT DATE_TRUNC(TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(DATA, "$.createdAt") AS INT)), DAY) AS creation_day
-    FROM `<PROJECT_NAME>.firestore_export.feedback_raw_latest`
+    FROM `firestore_export.feedback_raw_latest`
     WHERE   JSON_EXTRACT_SCALAR(DATA, "$.status") = "pending" and JSON_EXTRACT_SCALAR(DATA, "$.archived")= "0"
 )
 
@@ -12,5 +12,5 @@ CASE
     WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=14 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<30 THEN "Requests between 2 weeks and one month old"
     WHEN date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)>=0 and date_diff(CURRENT_TIMESTAMP(),creation_day,DAY)<14 THEN "Requests between 0 and 2 weeks old"
 END AS age
-from requests_feedbacks
-group by creation_day
+FROM requests_feedbacks
+GROUP BY creation_day
