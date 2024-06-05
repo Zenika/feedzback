84474b584f9e59fe664ada95e3438cc269b80346
Stéphane Francel <stephane.francel@zenika.com>
2024-06-05 11:38:05 +0200

    Improve mobile menu

--- client/src/app/header/header.animations.ts
+++ client/src/app/header/header.animations.ts
@@ -7,4 +7,8 @@ export const headerAnimations = [
       animate('500ms 500ms cubic-bezier(0.77, 0.2, 0.05, 1)'),
     ]),
   ]),
+  trigger('fadeInOut', [
+    transition('void => *', [style({ opacity: 0 }), animate('500ms cubic-bezier(0.77, 0.2, 0.05, 1)')]),
+    transition('* => void', [animate('500ms cubic-bezier(0.77, 0.2, 0.05, 1)'), style({ opacity: 0 })]),
+  ]),
 ];
