9971ede000dd5dd181346e63bc74b8becec0b31d
Stéphane Francel <stephane.francel@zenika.com>
2024-07-01 14:53:16 +0200

    Rename "app-division" to "app-slash" and improve its api

---
+++ client/src/app/shared/slash/slash.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-slash',
+  host: { class: 'app-slash' },
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './slash.component.html',
+  styleUrl: './slash.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class SlashComponent {}
