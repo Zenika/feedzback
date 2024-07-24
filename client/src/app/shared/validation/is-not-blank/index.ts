a83d6885ec6c81e9ef04c8bdaec8174444f91b9b
Stéphane Francel <stephane.francel@zenika.com>
2024-07-25 01:25:04 +0200

    client,server: fix blank spaces in "positive" and "negative" feedback properties (#634)

---
+++ client/src/app/shared/validation/is-not-blank/index.ts
@@ -0,0 +1,1 @@
+export * from './is-not-blank.validator';
--- server/src/feedback/feedback.dto.ts
+++ server/src/feedback/feedback.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
-import { IsBoolean, IsEmail, IsIn, IsString, MaxLength } from 'class-validator';
+import { IsBoolean, IsEmail, IsIn, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { FeedbackDraftType, FeedbackListType, FeedbackRequestDraftType } from './feedback-db';
 import { LARGE_MAX_LENGTH, MEDIUM_MAX_LENGTH, SMALL_MAX_LENGTH } from './feedback.config';
 
@@ -22,9 +22,17 @@ export class FeedbackArchiveRequestDto {
 export class GiveRequestedFeedbackDto {
   @IsString() token!: string;
 
-  @IsString() @MaxLength(LARGE_MAX_LENGTH) positive!: string;
+  @IsString()
+  @Transform((params) => (params.value as string)?.trim())
+  @IsNotEmpty()
+  @MaxLength(LARGE_MAX_LENGTH)
+  positive!: string;
 
-  @IsString() @MaxLength(LARGE_MAX_LENGTH) negative!: string;
+  @IsString()
+  @Transform((params) => (params.value as string)?.trim())
+  @IsNotEmpty()
+  @MaxLength(LARGE_MAX_LENGTH)
+  negative!: string;
 
   @IsString() @MaxLength(MEDIUM_MAX_LENGTH) comment!: string;
 }
@@ -32,9 +40,17 @@ export class GiveRequestedFeedbackDto {
 export class GiveFeedbackDto {
   @IsEmail() @Transform((params) => (params.value as string).toLowerCase()) receiverEmail!: string;
 
-  @IsString() @MaxLength(LARGE_MAX_LENGTH) positive!: string;
+  @IsString()
+  @Transform((params) => (params.value as string)?.trim())
+  @IsNotEmpty()
+  @MaxLength(LARGE_MAX_LENGTH)
+  positive!: string;
 
-  @IsString() @MaxLength(LARGE_MAX_LENGTH) negative!: string;
+  @IsString()
+  @Transform((params) => (params.value as string)?.trim())
+  @IsNotEmpty()
+  @MaxLength(LARGE_MAX_LENGTH)
+  negative!: string;
 
   @IsString() @MaxLength(MEDIUM_MAX_LENGTH) comment!: string;
 
