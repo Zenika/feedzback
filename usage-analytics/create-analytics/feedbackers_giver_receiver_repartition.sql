d244031477bc601d14a33b228dd02dcc421c2a07
Yasmine <yasmine.doghri@zenika.com>
2024-10-02 13:25:42 +0200

    corriger la requete pour récupérer le nombre de givers et receivers sur la période séléctionnée

--- usage-analytics/create-analytics/feedbackers_giver_receiver_repartition.sql
+++ usage-analytics/create-analytics/feedbackers_giver_receiver_repartition.sql
@@ -1,18 +1,9 @@
 WITH feedbacks AS (
    SELECT 
+   DATE_TRUNC(TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(DATA, "$.createdAt") AS INT)), DAY) AS day,
        JSON_EXTRACT_SCALAR(DATA, "$.giverEmail") AS giverEmail,
        JSON_EXTRACT_SCALAR(DATA, "$.receiverEmail") AS receiverEmail
    FROM firestore_export.feedback_raw_latest
-
-),
-all_emails_feedbackers AS (
-    SELECT giverEmail AS email FROM feedbacks 
-    UNION DISTINCT
-    SELECT receiverEmail AS email FROM feedbacks
 )
-
-SELECT 
-    COUNT(DISTINCT giverEmail) AS total_givers,            -- Nombre total de givers uniques
-    COUNT(DISTINCT receiverEmail) AS total_receivers,      -- Nombre total de receivers uniques
-    COUNT(DISTINCT email) AS total_feedbackers              -- Nombre total de feedbackers (givers + receivers sans doublons)
-FROM feedbacks, all_emails_feedbackers;
+SELECT DISTINCT *
+FROM feedbacks
