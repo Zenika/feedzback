fdcf3825ae83e4dd262962ad1a7451165cc108fb
Yasmine <yasmine.doghri@zenika.com>
2024-10-04 15:24:19 +0200

    exclure les givers et les receivers des feedbacks non répondus

--- usage-analytics/create-analytics/feedbackers_giver_receiver_repartition.sql
+++ usage-analytics/create-analytics/feedbackers_giver_receiver_repartition.sql
@@ -3,7 +3,9 @@ WITH feedbacks AS (
    DATE_TRUNC(TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(DATA, "$.createdAt") AS INT)), DAY) AS day,
        JSON_EXTRACT_SCALAR(DATA, "$.giverEmail") AS giverEmail,
        JSON_EXTRACT_SCALAR(DATA, "$.receiverEmail") AS receiverEmail
+
    FROM firestore_export.feedback_raw_latest
 )
 SELECT DISTINCT *
 FROM feedbacks
+WHERE JSON_EXTRACT_SCALAR(DATA, "$.status") = "done"
