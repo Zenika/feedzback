1c99e16a2a61a173266b39d1c4b5cd86c18edf46
Stéphane Francel <stephane.francel@zenika.com>
2024-10-07 00:55:00 +0200

    feat(server): consolidate feedback stats

--- server/src/feedback/feedback-stats/feedback-stats.service.ts
+++ server/src/feedback/feedback-stats/feedback-stats.service.ts
@@ -1,48 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { FeedbackDbService } from '../feedback-db/feedback-db.service';
-
-type FeedbackHistory = {
-  createdAt: number;
-  updatedAt: number;
-  requested: boolean;
-  shared: boolean;
-  status: 'pending' | 'done';
-};
+import { FeedbackHistory } from './feedback-stats.types';
+import { buildHistoryByMonth, buildHistoryByMonthStats, buildHistoryStats } from './feedback-stats.utils';
 
 @Injectable()
 export class FeedbackStatsService {
-  /**
-   * List of unique users who have given at least 1 feedback
-   */
-  private giverEmailList = new Set<string>();
-
-  /**
-   * List of unique users who have received at least 1 feedback
-   */
-  private receiverEmailList = new Set<string>();
-
-  /**
-   * List of unique users who have given of received at least 1 feedback
-   */
-  private allEmailList = new Set<string>();
-
   private history = new Map<string, FeedbackHistory>();
 
   constructor(private feedbackDbService: FeedbackDbService) {
     this.feedbackDbService.onFeedbackChanges((feedbacks) => {
-      feedbacks.forEach(({ giverEmail, receiverEmail, requested, status }) => {
-        // For feedback requests, the `giverEmail` is not to be taken into account until he has replied.
-        if (!requested || status === 'done') {
-          this.giverEmailList.add(giverEmail);
-          this.allEmailList.add(giverEmail);
-        }
-
-        this.receiverEmailList.add(receiverEmail);
-        this.allEmailList.add(receiverEmail);
-      });
-
-      feedbacks.forEach(({ id, createdAt, updatedAt, requested, shared, status }) =>
-        this.history.set(id, { createdAt, updatedAt, requested, shared, status }),
+      feedbacks.forEach(({ id, giverEmail, receiverEmail, shared, requested, status, createdAt, updatedAt }) =>
+        this.history.set(id, { giverEmail, receiverEmail, shared, requested, status, createdAt, updatedAt }),
       );
     });
   }
@@ -50,11 +18,11 @@ export class FeedbackStatsService {
   // Note that the first call to this method will trigger service creation and data supply.
   // As the method's return is synchronous, it's possible that there will be no data for the first few calls.
   getStats() {
+    const history = Array.from(this.history.values());
+
     return {
-      numberOfUniqueGivers: this.giverEmailList.size,
-      numberOfUniqueReceivers: this.receiverEmailList.size,
-      numberOfUniqueUsers: this.allEmailList.size,
-      history: Array.from(this.history.values()),
+      summary: buildHistoryStats(history),
+      details: buildHistoryByMonthStats(buildHistoryByMonth(history)),
     };
   }
 }
---
+++ server/src/feedback/feedback-stats/feedback-stats.types.ts
@@ -0,0 +1,24 @@
+export type FeedbackHistory = {
+  giverEmail: string;
+  receiverEmail: string;
+  shared: boolean;
+  requested: boolean;
+  status: 'pending' | 'done';
+  createdAt: number;
+  updatedAt: number;
+};
+
+export type FeedbackHistoryStats = {
+  // User stats
+  uniqueGivers: number;
+  uniqueReceivers: number;
+  uniqueUsers: number;
+
+  // Feedback stats
+  spontaneousFeedback: number;
+  requestedFeedbackDone: number;
+  requestedFeedbackPending: number;
+  sharedFeedback: number;
+};
+
+export type FeedbackMonthHistoryStats = { month: string } & FeedbackHistoryStats;
