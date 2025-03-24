a9887faa6217601a8cce1e8adb9b2c2b3fe07779
Stéphane Francel <stephane.francel@zenika.com>
2025-03-25 00:35:27 +0100

    Improve buttons a11y and the design of the burger button

--- client/src/app/header/burger/burger.component.ts
+++ client/src/app/header/burger/burger.component.ts
@@ -1,14 +1,16 @@
 import { Component, ViewEncapsulation, model } from '@angular/core';
+import { MatRipple } from '@angular/material/core';
 
 @Component({
   selector: 'app-burger',
   host: {
     class: 'app-burger',
-    '[tabIndex]': '0',
     '[class.app-burger--active]': 'active()',
+    '[tabIndex]': '0',
     '(click)': 'toggle()',
     '(keyup.enter)': 'toggle()',
   },
+  hostDirectives: [MatRipple],
   templateUrl: './burger.component.html',
   styleUrl: './burger.component.scss',
   encapsulation: ViewEncapsulation.None,
