dcb3de0d21ff49d703c3347ce85cc76bc8238cef
Stéphane Francel <stephane.francel@zenika.com>
2025-08-24 01:36:48 +0200

    client (fix): sign-in.page.ts (e2e)

--- client/e2e/pages/sign-in.page.ts
+++ client/e2e/pages/sign-in.page.ts
@@ -11,7 +11,7 @@ export class SignInPage {
     await this.page.getByRole('button', { name: 'Se connecter avec identifiants' }).click();
     await this.page.getByLabel('Email').fill(persona);
     await this.page.getByLabel('Mot de passe', { exact: true }).fill('zenika');
-    await this.page.getByRole('button', { name: 'Se connecter' }).click();
+    await this.page.getByRole('button', { name: 'Se connecter', exact: true }).click();
 
     await this.page.waitForURL('/fr/home');
   }
