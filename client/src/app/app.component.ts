6440fb5f94dfdf1cf3afc05a17876f3d6f5e3003
Stéphane Francel <stephane.francel@zenika.com>
2025-12-29 03:22:15 +0100

    client: remove "no-manager-dialog" component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LayoutModule } from './layout';
-import { NoManagerDialogComponent } from './no-manager-dialog/no-manager-dialog.component';
 import { VersionComponent } from './version/version.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, LayoutModule, NoManagerDialogComponent, VersionComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, LayoutModule, VersionComponent],
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
 })
