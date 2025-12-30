9642f7fe3e0ec2ffcba0dd43a4a88dde54be9621
Stéphane Francel <stephane.francel@zenika.com>
2025-12-30 01:11:46 +0100

    client: switch to Vitest and latest tsconfig configuration

---
+++ client/src/dummy.spec.ts
@@ -0,0 +1,7 @@
+// This test is just here to prevent `npm test` to fail because of no test provided...
+
+describe('Dummy', () => {
+  it('should create', () => {
+    expect(true).toBeTruthy();
+  });
+});
